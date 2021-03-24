19823bc19cfe299677e91639bee79c04f9f0526c
Vynter <amine-cheraitia@hotmail.com>
2021-03-24 17:05:32 +0100

    cmt

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'test' => \App\Http\Middleware\TestMiddleware::class, //cemme qque j'ai ajouté
+        'test' => \App\Http\Middleware\TestMiddleware::class, //celle que j'ai ajouté
     ];
 
     /**
