26f817733a16efd4f995a326f20445a6f1ce09d0
Vynter <amine-cheraitia@hotmail.com>
2021-03-31 17:39:06 +0200

    --

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -8,10 +8,10 @@ Client #{{$client->name}}
 <p><strong>Email :</strong>{{ $client->email }}</p>
 <p><strong>Entreprise :</strong>{{ $client->entreprise->name }}</p>
 <p><strong>Status :</strong>{{ $client->status }}</p>
-<!--if le client a une image on l'affiche-->
+<!--if le client a une image on l'affiche -->
 
 @if ($client->img)
-<img src="{{asset('storage/'.$client->img)}}" width="300" alt="client-avatar" class="img-thumbnail">
+<img src="{{asset('storage/'.$client->img)}}" width="300"  alt="client-avatar" class="img-thumbnail">
 @endif
 
 @endsection
